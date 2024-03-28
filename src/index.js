a45cc380e324ac196bc028b3c97c8452f0dbbb0c
vyazemskaya <vyazemskayanna@gmail.com>
2024-03-28 15:39:20 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './index.scss';
+
+// COMPONENTS
+import './js/components/burger';
+import './js/components/header';
+import './js/components/form';
+import './js/components/modal';
+
+// UTILS
+import './js/utils/jquery.mask';
+import './js/utils/jquery.twentytwenty';
+
+// PAGES
+import './js/pages/home-page';
+import './js/pages/news';
+import './js/pages/drilling-page';
+import './js/pages/well_cementing';
+import './js/pages/mpd';
+import './js/pages/about_us';
+import './js/pages/career';
+
+//planet
+import './js/components/planet';
